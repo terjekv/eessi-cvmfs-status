27bcdb01bb7692af9c6e24f3fbfcf1bc0a7589a9
Terje Kvernes <terje@kvernes.no>
2022-02-09 10:05:16 +0100

    Fixed external javascript?

--- status.js
+++ status.js
@@ -6,3 +6,24 @@ function open_close(myid) {
     el.style.display = 'none';
   }
 }
+
+function open_stratum0() {
+    open_close('stratum0')
+}
+
+function open_stratum1() {
+    open_close('stratum1')
+}
+
+function open_repositories() {
+    open_close('repositories')
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('stratum0_handler')
+            .addEventListener('click', open_stratum0);
+    document.getElementById('stratum1_handler')
+            .addEventListener('click', open_stratum1);
+    document.getElementById('repositories_handler')
+            .addEventListener('click', open_repositories);
+});
